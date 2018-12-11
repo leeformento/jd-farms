7b5e032f2bf5061f7045bb68202def7876faea22
Lee Len Formento <leebatef@gmail.com>
2018-12-11 13:02:15 -0500

    ayaw gumana update. copy paste luma code

--- notes/src/components/SingleNote.js
+++ notes/src/components/SingleNote.js
@@ -4,76 +4,45 @@ import { withRouter } from 'react-router-dom';
 import { fetchNote, deleteNote, updateNote } from '../actions';
 import Note from './Note';
 import Edit from './Edit';
-import { Button, Modal, ModalHeader, ModalBody } from 'reactstrap';
-
 
 
  class SingleNote extends Component {
     constructor(props){
         super(props);
         this.state = {
-            updateActive: false,
-            modal: false
+            updateActive: false
         }
     }
-
     componentDidMount(){
         this.props.fetchNote(this.props.match.params.id);
     }
 
-
     toggleUpdate = () => {
-        this.setState((prevState) => { 
-            return {updateActive: !prevState.updateActive}
-        });
-    }
-
-    toggleModal = () => {
-        this.setState((prevState) => {
-            return {modal: !prevState.modal};
-        });
-    }
-     deleteNote = () => {
-        this.props.deleteNote(this.props.match.params.id, this.props.history)
-        
+        this.setState({updateActive: true});
     }
 
      render() {
         console.log(this.props.note);
-        if (!this.props.fetched){
-            return(<div></div>)
+        if (this.props.fetching){
+            return (<div></div>)
         }
         return (
-            <div className="single-note-view">
-                 <Note title={this.props.note.title} 
+            <div>
+                <Note title={this.props.note.title} 
                 content={this.props.note.textBody} />
-                <Button onClick={this.toggleUpdate} color="primary">Update</Button>
-                <Button onClick={this.toggleModal} color="danger">Delete</Button>
-
+                <button onClick={this.toggleUpdate}>Update</button>
+                <button onClick={() => this.props.deleteNote(this.props.match.params.id)}>X</button>
                 {this.state.updateActive !== false
-                    ? <Edit onCancel={this.toggleUpdate} 
-                    title={this.props.note.title} 
-                    content={this.props.note.textBody} 
-                    updateNote={this.props.updateNote}
-                     id={this.props.match.params.id} />
+                    ? <Edit updateNote={this.props.updateNote} id={this.props.match.params.id} />
                     : null
                 }
-                
-                <Modal isOpen={this.state.modal} toggle={this.toggleModal}>
-                    <ModalHeader toggle={this.toggleModal}>Are you sure you want to delete this note?</ModalHeader>
-                    <ModalBody>
-                        <Button color="danger" onClick={this.deleteNote}>Delete</Button>{' '}
-                        <Button color="secondary" onClick={this.toggleModal}>Cancel</Button>
-                    </ModalBody>
-                </Modal>
-                {/* <Link to="/">Back</Link> */}
                 </div>
         )
     }
 }
  const mapStateToProps = state => {
     return {
-        fetching: state.fetchingNotes,
+        fetching: state.fetching,
         fetched: state.fetched,
         note: state.note,
         error: state.error
