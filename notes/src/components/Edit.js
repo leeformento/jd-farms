bb01a639fca9f426de94a07c1fdf84a4dc5fcdac
Lee Len Formento <leebatef@gmail.com>
2018-12-11 12:59:15 -0500

    kung sakali lang hehe

--- notes/src/components/Edit.js
+++ notes/src/components/Edit.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react'
-import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
 
  class Edit extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            title: this.props.title,
-            content: this.props.content
+            title: '',
+            content: ''
         }
     }
      handleChange = event => {
@@ -19,28 +18,20 @@ import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
             title: this.state.title,
             content: this.state.content
         }
-        this.props.updateNote(updatedNote,this.props.history);
+        this.props.updateNote(updatedNote);
         this.setState({
             title: '',
             content: ''
         })
-        
     }
 
      render() {
         return (
-            <Form onSubmit={this.updateNote} className="note-form">
-                <FormGroup>
-                    <Label for="title">Update Title</Label>
-                    <Input type="text" name="title" id="title" onChange={this.handleChange} value={this.state.title} required></Input>
-                </FormGroup>
-                <FormGroup>
-                    <Label for="content"> Update Note</Label>
-                    <Input type="textarea" name="content" id="content" onChange={this.handleChange} value={this.state.content} required></Input>
-                </FormGroup>
-                <Button color="primary">Update!</Button>
-                <Button type="button" onClick={this.props.onCancel} color="danger">Cancel</Button>
-            </Form>
+            <form onSubmit={this.updateNote}>
+                <input type="text" name="title" onChange={this.handleChange} value={this.state.title} required></input>
+                <textarea name="content" onChange={this.handleChange} value={this.state.content} required></textarea>
+                <button>Update!</button>
+            </form>
         )
     }
 }
